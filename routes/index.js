d576cdc03a19844dcc2be141240b730227cb686d
QuartzShard <matthew.burgess50@gmail.com>
2020-08-27 03:29:05 +0000

    added mock authform

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
+const path = require('path'); 
+require('dotenv').config({ path: path.join(__dirname, '../.env')})
 var express = require('express');
 var router = express.Router();
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Drink and Vibe Engine Web Console' });
 });
+ /* POST login */
+router.post('/',function(req,res,next) {
+  if (req.body.key == process.env.KEY) {
+    res.redirect('/menu')
+  }
+  res.redirect('/')
+})
 
+router.get('/menu', function(req,res,next) {
+  res.render('menu',{ title: 'Drink and Vibe Engine Web Console' })
+})
 module.exports = router;
