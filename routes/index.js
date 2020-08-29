55aa0ed7f2c389569c3f13e7ed0a7802ad4c0a78
QuartzShard <matthew.burgess50@gmail.com>
2020-08-29 03:58:22 +0000

    Added sanitization and logout route

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,27 @@ require('dotenv').config({ path: path.join(__dirname, '../.env')})
 var express = require('express');
 var router = express.Router();
 var passport = require('passport')
+var {body} = require('express-validator')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Drink and Vibe Engine Web Console' });
 });
+
  /* POST login */
-router.post('/',passport.authenticate('local',{ 
+router.post('/',[body(['username','password']).escape().exists()],passport.authenticate('local',{ 
   failureRedirect:'/',
   successRedirect:'/app/menu'
 }),function(req,res,next) {
   res.end()
+})
+
+/**
+ * Log out route, redirects to login
+ */
+router.get('/logout',function(req,res,next){
+  req.logOut()
+  res.redirect('/')
 })
 
 module.exports = router;
