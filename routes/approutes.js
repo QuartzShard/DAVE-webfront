635a5d4678d87da10d85a9c074832ef5bf87daf4
QuartzShard <matthew.burgess50@gmail.com>
2020-08-29 03:58:46 +0000

    new user form

--- routes/approutes.js
+++ routes/approutes.js
@@ -3,6 +3,8 @@ require('dotenv').config({ path: path.join(__dirname, '../.env')})
 var express = require('express');
 var router = express.Router();
 var passport = require('passport')
+var {body} = require('express-validator')
+var db = require('../db/db')
 
 /** 
  * Redirect any unauthenticated users away from here
@@ -19,4 +21,13 @@ router.get('/menu', function(req,res,next) {
       res.render('menu',{ title: 'Drink and Vibe Engine Web Console' }) 
 })
 
+router.get('/newuser', function(req,res,next) {
+  res.render('newUser',{ title: 'Drink and Vibe Engine Web Console' }) 
+})
+
+router.post('/newuser',[body(['username','password']).escape().exists()], function(req,res,next){
+  var user = db.get('users')
+  var newUser = new user({username:req.body.username,password:req.body.password}) 
+  newUser.save().then(res.redirect('/app/menu'))
+})
 module.exports = router;
